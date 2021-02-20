b18819fdb7bd9c3bb52b64638cf6b9294f52b219
Neendo_Njuguna <njugunaneendo@gmail.com>
2021-02-20 23:16:49 +0300

    Assignment 1

---
+++ src/Assignment 1.kt
@@ -0,0 +1,13 @@
+fun main() {
+val name=("dahlia natalia")
+    print(name)
+    var age= 12
+    print (age)
+    val phoneNumber= 712345678
+    print(phoneNumber)
+    var weight= 56.98765
+    print (weight)
+    val citizen= ("true")
+    val student= arrayOf("dahlia natalia", 12, 712345678, 56.98765,"true")
+    print(student)
+}
